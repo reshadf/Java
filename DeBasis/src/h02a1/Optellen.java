db6166ee7f67c230ae8060fa09bb1d5c11b615ef
Reshad Farid <reshadfar@gmail.com>
2012-09-13 15:43:31 +0200

    added new class

--- DeBasis/src/h02a1/Optellen.java
+++ DeBasis/src/h02a1/Optellen.java
@@ -1,23 +1,23 @@
-package h02;
+package h02a1;
 
 import javax.swing.*;
 
-public class Verwissel {
+public class Optellen {
 	
 	JFrame venster = null;
 	
-	public Verwissel() {
+	public Optellen() {
 		JFrame venster = new JFrame();
-		venster.setSize(500, 200);
+		venster.setSize(500, 100);
 		venster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		venster.setTitle("Verwissel opdracht hoofdstuk 2");
 		venster.setLocation(500, 500);
-		venster.add(new VerwisselPaneel());
+		venster.add(new OptelPaneel());
 		venster.setVisible(true);
 	}
 	
 	public static void main(String[] args) {
-		new Verwissel();
+		new Optellen();
 	}
 
 }
