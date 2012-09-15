8e19be4866e4316643394538ea87b9c4a9edd9af
Reshad Farid <reshadfar@gmail.com>
2012-09-15 19:14:44 +0200

    added try and catch block

--- DeBasis/src/h02a3/RechthoekenPaneel.java
+++ DeBasis/src/h02a3/RechthoekenPaneel.java
@@ -6,10 +6,10 @@ import java.awt.*;
 public class RechthoekenPaneel extends JPanel {
 	
 	//teken rechthoeken
-	public void paintcomponent(Graphics g) {
+	public void paintComponent(Graphics g) {
 		
 		super.paintComponent( g );
-		g.fillRect(110, 110, 30, 30);
 		g.setColor(Color.red);
+		g.fillRect(110, 110, 30, 30);
 	}
 }
