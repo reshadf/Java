34a87df9efcd177521dc05d8ee7eb630cdfce678
Reshad Farid <reshadfar@gmail.com>
2012-09-15 18:36:05 +0200

    added try and catch block

--- DeBasis/src/h02a3/Rechthoeken.java
+++ DeBasis/src/h02a3/Rechthoeken.java
@@ -27,7 +27,13 @@ public class Rechthoeken {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		new Rechthoeken();
+		try { 
+			new Rechthoeken();
+		}
+		
+		catch(RuntimeException e) {
+			JOptionPane.showMessageDialog(null, "error");
+		}
 
 	}
 
