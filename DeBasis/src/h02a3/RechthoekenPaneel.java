3adc717d710907da5b392eaa9a264d9b0be04852
Reshad Farid <reshadfar@gmail.com>
2012-09-15 18:20:04 +0200

    changed RechthoekenPaneel

--- DeBasis/src/h02a3/RechthoekenPaneel.java
+++ DeBasis/src/h02a3/RechthoekenPaneel.java
@@ -7,11 +7,9 @@ public class RechthoekenPaneel extends JPanel {
 	
 	//teken rechthoeken
 	public void paintcomponent(Graphics g) {
-		int width = getWidth();
-		int height = getHeight();
+		
 		super.paintComponent( g );
-		//g.fillRect(110, 110, 30, 30);
+		g.fillRect(110, 110, 30, 30);
 		g.setColor(Color.red);
-		g.drawOval(0, 0, width, height);
 	}
 }
