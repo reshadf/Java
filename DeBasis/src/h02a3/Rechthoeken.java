fb28b10d2247e552961adf00c4abac65e3e6199e
Reshad Farid <reshadfar@gmail.com>
2012-09-15 17:27:37 +0200

    added pixels in venster

---
+++ DeBasis/src/h02a3/Rechthoeken.java
@@ -0,0 +1,32 @@
+package h02a3;
+
+import javax.swing.*;
+
+public class Rechthoeken {
+
+	JFrame venster = null;
+	
+	public Rechthoeken() {
+		// TODO Auto-generated constructor stub
+		
+		JFrame venster = new JFrame();
+		venster.setSize(500, 200);
+		venster.setLocation(500, 500);
+		venster.setTitle("Pixels in het venster");
+		venster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		venster.add(new RechthoekenPaneel());
+		venster.setVisible(true);
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		new Rechthoeken();
+
+	}
+
+}
---
+++ DeBasis/src/h02a3/RechthoekenPaneel.java
@@ -0,0 +1,12 @@
+package h02a3;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RechthoekenPaneel extends JPanel {
+	//teken rechthoeken
+	public void paintcomponent(Graphics g) {
+		super.paintComponent(g);
+		g.fillRect(110, 110, 20, 20);
+	}
+}
