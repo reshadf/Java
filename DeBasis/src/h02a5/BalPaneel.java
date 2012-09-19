42ff4ec01db35132e75070d8d7e13bf1d3f6f049
Reshad Farid <reshadfar@gmail.com>
2012-09-19 13:04:31 +0200

    tekenbal

--- DeBasis/src/h02a5/BalPaneel.java
+++ DeBasis/src/h02a5/BalPaneel.java
@@ -2,30 +2,37 @@ package h02a5;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class BalPaneel extends JPanel {
+public class BalPaneel extends JPanel implements ActionListener {
 
-//	public JTextField grootte; // invoerbox veldgrootte
-//	private JButton teken; // Teken button
-//	
-//	
-//	public BalPaneel() {
-//		
-//		teken = new JButton("teken bal");
-//		teken.addActionListener(new TekenBal());
-//		grootte = new JTextField(3);
-//		grootte.addActionListener(new TekenBal());
-//		
-//		add(grootte);
-//		add(teken);
-//	}
+	public JTextField grootte; // invoerbox veldgrootte
+	private JButton teken; // Teken button
+	
+	
+	public BalPaneel() {
+		
+		teken = new JButton("teken bal");
+		teken.addActionListener(this);
+		grootte = new JTextField(3);
+		grootte.addActionListener(this);
+		
+		add(grootte);
+		add(teken);
+	}
 	
 	TekenBal bal = new TekenBal();
 	
 	public void paintComponent(Graphics g) {
 		
 		super.paintComponent(g);
-		bal.tekenBal(g, 50, 50, 20);
+		bal.tekenBal(g, 100, 100, 40);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		
+		
 	}
 	
 }
--- DeBasis/src/h02a5/TekenBal.java
+++ DeBasis/src/h02a5/TekenBal.java
@@ -7,6 +7,11 @@ public class TekenBal {
 	public void tekenBal(Graphics g, int x, int y, int grootte) {
 		
 		g.drawOval(x, y, grootte, grootte);
+		g.setColor(Color.orange);
+		g.fillOval(x, y, grootte, grootte);
+		g.setColor(Color.black);
+		g.drawOval(x + grootte / 4, y, grootte / 2, grootte);
+		
 	}
 
 }
