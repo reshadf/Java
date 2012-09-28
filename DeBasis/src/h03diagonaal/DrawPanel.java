81bb6dd2c4247de51f295a09f50ab5a3306e0d5d
reshadf <reshadfar@gmail.com>
2012-09-28 20:27:44 +0200

    Diagonal lines finished

--- DeBasis/src/h03diagonaal/DrawPanel.java
+++ DeBasis/src/h03diagonaal/DrawPanel.java
@@ -30,14 +30,11 @@ public class DrawPanel extends JPanel implements ActionListener {
 		
 		int xPos = 0;
 		int yPos = getHeight() - (getHeight() / 2);
-		int localValue = value;
 		
-		for(int aantalLines = 0; aantalLines < 10; aantalLines++ ) {
-			line.drawLines(g, xPos, yPos + localValue, getWidth(), getHeight() - localValue );
-			localValue += localValue;
-			System.out.println(localValue);
-			System.out.println(aantalLines);
-		}
+		for(int aantalLines = 0; aantalLines < 20; aantalLines++ ) {
+			  line.drawLines(g, xPos,       yPos +        ((aantalLines + 1) * value), 
+			                    getWidth(), getHeight() - ((aantalLines + 1) * value) );
+			}
 		
 	}
 	
