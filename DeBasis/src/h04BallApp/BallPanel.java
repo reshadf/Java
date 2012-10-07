7648ed94c1c2037acca44d94320c4aec19863b8b
reshadf <reshadfar@gmail.com>
2012-10-07 13:25:41 +0200

    drawBall part 2

---
+++ DeBasis/src/h04BallApp/BallPanel.java
@@ -0,0 +1,42 @@
+package h04BallApp;
+
+import javax.swing.*;
+import java.awt.event.*;
+import h03Bal.DrawBal;
+
+public class BallPanel extends JPanel implements ActionListener, KeyListener {
+
+	private JButton naarLinks, naarRechts;
+	DrawBal bal = new DrawBal();
+	
+	public BallPanel() {
+		
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+		
+	}
+
+}
---
+++ DeBasis/src/h04BallApp/Main.java
@@ -0,0 +1,13 @@
+package h04BallApp;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
