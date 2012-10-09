9ff263551a621491413be0542ba44b859dc92e91
reshadf <reshadfar@gmail.com>
2012-10-09 23:29:04 +0200

    Ghost finished

--- DeBasis/src/h04PacMan/DrawPacMan.java
+++ DeBasis/src/h04PacMan/DrawPacMan.java
@@ -6,20 +6,41 @@ import java.lang.Math;
 public class DrawPacMan {
 	
 	int angle;
+	
+	/*
+	 * Drawing of Pacman
+	 */
 
 	public void drawPacMan(Graphics g, int x, int y, int radius, Color color) {
 		
+		//pacman body
 		angle = (int) (20 * (Math.sin((x + y)*2*Math.PI/50) + 1)); 
 		g.setColor(color);
-		g.fillArc(x, y, 50, 50, angle/2, 360-angle);
+		g.fillArc(x, y, radius, radius, angle/2, 360-angle);
+		
+		// pacman eyes
+		g.setColor(Color.black);
+		g.fillOval(x + 15 ,y + 5 , radius / 4, radius / 4); // drawing eye;
+
 		
 	}
+	
+	/*
+	 * Drawing of Pacman eating Ghost
+	 */
 
 	public void drawGhost(Graphics g, int x, int y, int radius, Color color) {
 		
-		g.setColor(color); // color
-		g.fillRect(x, y, radius, radius);
-		g.drawRect(x, y, radius, radius);
+		// ghost body
+		g.setColor(color);
+		g.fillArc (x, y, radius, radius, 0, 180); 
+
+		// ghost eyes
+		g.setColor(color.black);
+		g.drawOval(x + 20,y + 15, 10,5); 
+		g.fillOval(x + 20,y + 15, 5,5); 
+		g.drawOval(x + 40,y +15, 10,5);
+		g.fillOval(x + 40, y + 15, 5,5); 
 		
 	}
 
