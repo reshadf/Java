249754e00625c618154ede09f970857a95fae048
reshadf <reshadfar@gmail.com>
2012-10-09 23:03:39 +0200

    PacMan with mouth open/closing

--- DeBasis/src/h04PacMan/DrawPacMan.java
+++ DeBasis/src/h04PacMan/DrawPacMan.java
@@ -1,13 +1,17 @@
 package h04PacMan;
 
 import java.awt.*;
+import java.lang.Math;
 
 public class DrawPacMan {
+	
+	int angle;
 
 	public void drawPacMan(Graphics g, int x, int y, int radius, Color color) {
 		
+		angle = (int) (20 * (Math.sin((x + y)*2*Math.PI/50) + 1)); 
 		g.setColor(color);
-		g.fillArc(x,y,radius,radius,30,300);
+		g.fillArc(x, y, 50, 50, angle/2, 360-angle);
 		
 	}
 
