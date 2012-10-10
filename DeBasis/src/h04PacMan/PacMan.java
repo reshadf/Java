f1d5e2025c92e46fe5f637590d12f57d790808ca
Reshad Farid <reshadfar@gmail.com>
2012-10-10 12:41:02 +0200

    pacman

--- DeBasis/src/h04PacMan/PacMan.java
+++ DeBasis/src/h04PacMan/PacMan.java
@@ -13,7 +13,7 @@ public class PacMan extends JPanel implements ActionListener {
 	
 	private int horizontalePlaats = 250; // x location
 	private int verticalePlaats = 150; // y location
-	private int richtingEnSnelheid = +10; // RECHTS
+	private int richtingEnSnelheid = +10; // speed
 	private final int WACHTTIJD = 500; // DELAY
 	int diameter;
 	int waarde;
@@ -30,13 +30,13 @@ public class PacMan extends JPanel implements ActionListener {
 		return horizontalePlaats;
 	}
 	
-	// huidige y positie geven
+	// current y positie
 	
 	public int getVerPlaats() {
 		return verticalePlaats;
 	}
 	
-	// snelheid en richting
+	// speed and way;
 	public int getRichtingEnSnelheid() {
 		return richtingEnSnelheid;
 	}
@@ -45,12 +45,30 @@ public class PacMan extends JPanel implements ActionListener {
 	
 	public void setHorPlaats(int nieuweHorPlaats) {
 		
+		if(nieuweHorPlaats > getWidth()) {
+			
+			nieuweHorPlaats = 0;
+		}
+		else if(nieuweHorPlaats < 0) {
+			
+			nieuweHorPlaats = getWidth();
+		}
+		
 		horizontalePlaats = nieuweHorPlaats;
 		
 	}
 	
 	// new y position 
 	public void setVerPlaats(int nieuweVerPlaats) {
+		
+		if(nieuweVerPlaats > getHeight()) {
+			
+			nieuweVerPlaats = 0;
+		}
+		else if(nieuweVerPlaats < 0) {
+			
+			nieuweVerPlaats = getHeight();
+		}
 		
 		verticalePlaats = nieuweVerPlaats;
 		
@@ -62,14 +80,15 @@ public class PacMan extends JPanel implements ActionListener {
 		
 	}
 	
+	//movement
 	public void setBesturing(int besturing) {
 		
 		besturing = waarde;
 		
-		if(waarde == 1) {
+		if(waarde == 0) {
 			setVerPlaats( getVerPlaats() + richtingEnSnelheid);
 		}
-		else if(waarde == 0){
+		else if(waarde == 1){
 			setHorPlaats( getHorPlaats() + richtingEnSnelheid);
 		}
 	}
@@ -85,16 +104,24 @@ public class PacMan extends JPanel implements ActionListener {
 	
 	DrawPacMan pacman = new DrawPacMan();
 	DrawPacMan ghost1 = new DrawPacMan();
+	DrawPacMan ghost2 = new DrawPacMan();
 	
 	public void paintComponent(Graphics g) {
 		
 		super.paintComponent(g);
+		// pacman movement
 		diameter = 75;	
 		pacman.drawPacMan(g, getHorPlaats(), getVerPlaats(), diameter, Color.yellow);
 		
-	//	int[] ghostx, ghosty;
+		// ghosts movement
+		int g1x;
+		for(g1x = 0; g1x < 10; g1x++) {
+			
+			pacman.drawGhost(g, g1x, 40, diameter, Color.red);
+			
+		}
+		pacman.drawGhost(g, 170, 70, diameter, Color.blue);
 		
-		pacman.drawGhost(g, 50, 40, diameter, Color.red);
 		
 	}
 
