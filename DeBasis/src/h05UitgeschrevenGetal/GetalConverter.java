11205dd717ef9a4c599e2971976e4b6e3ed00234
reshadf <reshadfar@gmail.com>
2012-10-17 12:42:20 +0200

    Convert to letters

--- DeBasis/src/h05UitgeschrevenGetal/GetalConverter.java
+++ DeBasis/src/h05UitgeschrevenGetal/GetalConverter.java
@@ -1,30 +1,25 @@
 package h05UitgeschrevenGetal;
 
 import java.awt.event.*;
-import java.util.Random;
+import java.util.regex.*;
 
 import javax.swing.*;
-import java.util.*;
 
 public class GetalConverter extends JPanel implements ActionListener {
 	
 	private JLabel getal;
 	private JTextField invoer, uitvoer;
 	private int invoerWaarde;
+	private String convertedVal;
 	private JButton converteer;
 	
-	private int klein = 19;
-	String[] strings = new String[klein];
-    Random random = new Random();
-    
-    private int groot = 90;
-    String[] stringsGroot = new String[groot];
-    
-    int standardValue = 0;
+	String[] klein = new String[20];
+    String[] groot = new String[10];
 
 	public GetalConverter() {
 		
 		getal = new JLabel("geef een geheel getal");
+		add(getal);
 		
 		invoer = new JTextField(7);
 		invoer.addActionListener(this);
@@ -34,39 +29,46 @@ public class GetalConverter extends JPanel implements ActionListener {
 		converteer.addActionListener(this);
 		add(converteer);
 		
-		uitvoer = new JTextField(7);
+		uitvoer = new JTextField(20);
 		uitvoer.setEditable(false);
 		add(uitvoer);
 		
-		/*
-		 * kleine getallen
-		 */
+		klein[0] = "nul";
+		klein[1] = "een";
+		klein[2] = "twee";
+		klein[3] = "drie";
+		klein[4] = "vier";
+		klein[5] = "vijf";
+		klein[6] = "zes";
+		klein[7] = "zeven";
+		klein[8] = "acht";
+		klein[9] = "negen";
+		klein[10] = "tien";
+		klein[11] = "elf";
+		klein[12] = "twaalf";
+		klein[13] = "dertien";
+		klein[14] = "veertien";
+		klein[15] = "vijftien";
+		klein[16] = "zestien";
+		klein[17] = "zeventien";
+		klein[18] = "achtien";
+		klein[19] = "negentien";
 		
-		for(int i = 0; i < klein; i++) {
-			
-			strings[i] = "" + random.nextInt(klein);
-			
-		}
-		
-		
-		/*
-		 * grote getallen
-		 */
-		
-		for(int g = 0; g < groot; g += 10) {
-			
-			stringsGroot[g] = "" + random.nextInt(groot);
-			
-		}
-		
-		
+		groot[1] = "tien";
+		groot[2] = "twintig";
+		groot[3] = "dertig";
+		groot[4] = "veertig";
+		groot[5] = "vijftig";
+		groot[6] = "zestig";
+		groot[7] = "zeventig";
+		groot[8] = "tachtig";
+		groot[9] = "negentig";
 		
 	}
 	
 	public void setValue(int val) {
 		
 		 invoerWaarde = val;
-		 //System.out.println(invoerWaarde);
 	}
 	
 	public int getValue() {
@@ -75,31 +77,44 @@ public class GetalConverter extends JPanel implements ActionListener {
 		
 	}
 	
-//	public void convert(int waar) {
-//		
-//		for(int i=0;i<strings.length;i++){
-//			   if(strings[i].equals(invoerWaarde)){
-//				      System.out.println("index" + i);
-//				      System.out.println("valye" + strings[i]);
-//			   }
-//			}
-//		
-//	}
-	
-	public void convert(Integer waar) {
-	    for(int index=0;index<strings.length;index++){
-	       if(strings[index].equals(String.valueOf(waar))){
-	          System.out.println("index -> "+index);
-	       }
-	    }
+	public void Convert()
+	{
+	   int grootVal, kleinVal;
+	   String valueString;
+	   if(invoerWaarde < 100){
+	      // gevonden in arrays ?
+	      grootVal = (int)Math.floor(invoerWaarde / 10.0);
+	      kleinVal = invoerWaarde - (grootVal * 10);
+	      try{
+	    	  if(invoerWaarde < 20){
+	    		  valueString = klein[kleinVal] + "";
+	    	  }
+	    	  else {
+	    		  valueString = klein[kleinVal] + "en" + groot[grootVal];  
+	    	  }
+	    	  
+	    	  convertedVal = valueString;
+	    	  
+	      }
+	      catch(ArrayIndexOutOfBoundsException err){
+	          System.out.println("Error 1");
+	      }
+	   }
 	}
+	
+	public String getConvertedValue() {
+		
+		return convertedVal;
+		
+	}
+	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
 		setValue(Integer.parseInt(invoer.getText()));
-		convert(invoerWaarde);
-		System.out.println(invoerWaarde);
+		Convert();
+		uitvoer.setText(convertedVal);
 		
 	}
 
