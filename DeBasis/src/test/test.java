056e2370b3717f55f8ddb5660fe9d62ac7237b36
Reshad Farid <reshadfar@gmail.com>
2012-10-31 17:03:33 +0100

    nullpointer example

--- DeBasis/src/test/test.java
+++ DeBasis/src/test/test.java
@@ -6,18 +6,7 @@ public class test {
 	 * @param args
 	 */
 	  public static void main(String[] args) {
-		  
-		  String[] dagVanDeWeek = {"Zo", "Ma", "Di", "Wo", "Do", "Vr", "Za"};
-		  
-		  for(int x = 0; x < dagVanDeWeek.length - 1; x++) {
-			  dagVanDeWeek[x + 1] = dagVanDeWeek[x];
-		  }
-		  
-		  for(String dag : dagVanDeWeek) {
-			  System.out.println(dag);
-		  }
-		  
-		  
-		  
+		  String a = "10";
+		  System.out.println(a.toString());
 	  }   
 }
