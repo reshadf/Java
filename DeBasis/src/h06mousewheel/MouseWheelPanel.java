19700e6ce0921e2f02c6ff1023102bead9675f2a
Reshad Farid <reshadfar@gmail.com>
2012-12-04 22:03:09 +0100

    modified mousewheel class

--- DeBasis/src/h06mousewheel/MouseWheelPanel.java
+++ DeBasis/src/h06mousewheel/MouseWheelPanel.java
@@ -8,6 +8,9 @@ import java.awt.event.MouseWheelListener;
 public class MouseWheelPanel extends JPanel implements MouseWheelListener {
 	
 	private int grootte = 50;
+	private final String str = "";
+	private int x, y;
+	private int size;
 	
 	public MouseWheelPanel() {
 		this.addMouseWheelListener(this);
@@ -17,11 +20,15 @@ public class MouseWheelPanel extends JPanel implements MouseWheelListener {
 		super.paintComponent( g );
 		g.setColor( Color.YELLOW ); 
 		g.fillOval( 10, 10, grootte, grootte );
+		g.setColor(Color.black);
+		g.drawString(str + size, x, y);
 	}
-
-
+	
 	public void mouseWheelMoved( MouseWheelEvent e ) {
 		// TODO Auto-generated method stub
+		y = e.getY();
+		x = e.getX();
+		size = e.getWheelRotation();
 	    grootte += e.getWheelRotation();
 	    repaint(); 
 	}
